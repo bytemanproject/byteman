b297fb19e0cc25bbab3097eb3dee7894d747badd
Andrew Dinn <adinn@redhat.com>
2009-06-03 16:51:24 +0000

    added copyright where missing and updated in others

--- src/org/jboss/byteman/agent/adapter/RuleGeneratorAdapter.java
+++ src/org/jboss/byteman/agent/adapter/RuleGeneratorAdapter.java
@@ -27,6 +27,29 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2009, Red Hat Middleware LLC, and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*
+* @authors Andrew Dinn
+*/
 package org.jboss.byteman.agent.adapter;
 
 import java.util.ArrayList;
@@ -43,7 +66,7 @@ import org.objectweb.asm.commons.Method;
 import org.objectweb.asm.commons.TableSwitchGenerator;
 
 /**
- * A modified version of the asm GeneratorAdapter class which dispatches calls to methods of
+ * A modified version of the asm 3.0 GeneratorAdapter class which dispatches calls to methods of
  * MethodVisitor to this rather than to the encapsulated MethodVisitor instance in field mv.
  * Doing so gives the current instance a chance to observe all visit operations. Without it
  * the current instane only sees visit operations invoked directly by previous visitors in
