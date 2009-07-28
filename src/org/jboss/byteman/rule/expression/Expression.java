4a3283c0abfbad0db4d49acb255a597a563eb981
Andrew Dinn <adinn@redhat.com>
2009-07-28 08:43:41 +0000

    line numbers for expressions are now reported correctly -- fixes BYTEMAN-16

--- src/org/jboss/byteman/rule/expression/Expression.java
+++ src/org/jboss/byteman/rule/expression/Expression.java
@@ -53,7 +53,7 @@ public abstract class Expression extends RuleElement
         this.token = token;
         if (token != null) {
             this.charPos = token.getColumn();
-            this.line = rule.getLine() + token.getLine();
+            this.line = token.getLine();
         } else {
             this.charPos = 0;
             this.line = 0;
