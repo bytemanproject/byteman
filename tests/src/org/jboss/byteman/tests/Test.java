2031e243a15e6c63ab69248f4f48013e51b68f84
Andrew Dinn <adinn@redhat.com>
2009-09-15 16:39:25 +0000

    made successful test output less verbose

--- tests/src/org/jboss/byteman/tests/Test.java
+++ tests/src/org/jboss/byteman/tests/Test.java
@@ -79,9 +79,10 @@ public abstract class Test extends TestCase
         String output = getOutput();
         String expected = getExpected();
         if (!output.equals(expected)) {
-            fail("Test " + name + "fail" + "\n\n<expected>\n" + expected + "</expected>\n\n<log>\n" + output +"</log>\n");
+            fail("Test " + name + "failed\n" + "\n<expected>\n" + expected + "</expected>\n\n<log>\n" + output +"</log>\n");
         } else {
-            System.out.println("Test " + name + " success" + "\n\n<log>\n" + output + "</log>\n");
+            // System.out.println("Test " + name + " success" + "\n\n<log>\n" + output + "</log>\n");
+            System.out.println("Test " + name + " succeeded");
         }
 
         if (reset) {
