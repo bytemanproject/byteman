1d85f99e7b017f32e6d5423dde91a5e407a85820
Andrew Dinn <adinn@redhat.com>
2010-11-24 11:59:46 +0000

    extended new operator to alow array creation and added assignment for arrays -- fixes BYTEMAN-123 and BYTEMAN-137

--- src/org/jboss/byteman/rule/expression/ArrayExpression.java
+++ src/org/jboss/byteman/rule/expression/ArrayExpression.java
@@ -43,7 +43,7 @@ import java.lang.reflect.Array;
  * an expression which identifies an array reference.
  */
 
-public class ArrayExpression extends Expression
+public class ArrayExpression extends AssignableExpression
 {
 
     public ArrayExpression(Rule rule, Type type, ParseNode token, Expression arrayRef, List<Expression> idxList)
@@ -201,4 +201,158 @@ public class ArrayExpression extends Expression
 
     Expression arrayRef;
     List<Expression> idxList;
+
+    @Override
+    public Object interpretAssign(HelperAdapter helper, Object value) throws ExecuteException {
+        // evaluate the array expression then evaluate each index expression in turn up to the last one
+        // and dereference to access the array at that index. finally evaluate the final index expression
+        // and use it as the position at which to install the supplied value in the dereferenced array
+
+        try {
+            Object array = arrayRef.interpret(helper);
+            Type nextType = arrayRef.getType();
+            int count = idxList.size() - 1;
+            for (Expression expr : idxList) {
+                int idx = ((Number) expr.interpret(helper)).intValue();
+                if (array == null) {
+                    throw new ExecuteException("ArrayExpression.interpret : attempted array indirection through null value " + arrayRef.token.getText() + getPos());
+                }
+                if (count-- >  0)  {
+                    array = Array.get(array, idx);
+                    nextType = nextType.getBaseType();
+                } else {
+                    Array.set(array, idx, value);
+                }
+            }
+            return value;
+        } catch (ExecuteException e) {
+            throw e;
+        } catch (IllegalArgumentException e) {
+            throw new ExecuteException("ArrayExpression.interpret : failed to evaluate expression " + arrayRef.token.getText() + getPos(), e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ExecuteException("ArrayExpression.interpret : invalid index for array " + arrayRef.token.getText() + getPos(), e);
+        } catch (ClassCastException e) {
+            throw new ExecuteException("ArrayExpression.interpret : invalid index dereferencing array " + arrayRef.token.getText() + getPos(), e);
+        } catch (Exception e) {
+            throw new ExecuteException("ArrayExpression.interpret : unexpected exception dereferencing array " + arrayRef.token.getText() + getPos(), e);
+        }
+    }
+
+    @Override
+    public void compileAssign(MethodVisitor mv, CompileContext compileContext) throws CompileException {
+        Type valueType = arrayRef.getType().getBaseType();
+        int currentStack = compileContext.getStackCount();
+        boolean isTwoWords = (valueType.getNBytes() > 4);
+        int toPop = 0;
+        int size = (isTwoWords ? 2 : 1);
+
+        // value to be assigned is TOS and will already be coerced to the correct value type
+        // copy it so we can install the copy and leave the original as a a return value on the stack
+        if (isTwoWords) {
+            // [... val1 val2 ==> ... val1 val2 val1 val2]
+            mv.visitInsn(Opcodes.DUP2);
+        } else {
+            // [... val ==> ... val val]
+            mv.visitInsn(Opcodes.DUP);
+        }
+        compileContext.addStackCount(size);
+
+        // compile load of array reference -- adds 1 to stack height
+        arrayRef.compile(mv, compileContext);
+        // for each index expression compile the expression and the do an array load
+        Iterator<Expression> iterator = idxList.iterator();
+
+        while (iterator.hasNext()) {
+            Expression idxExpr = iterator.next();
+            if (iterator.hasNext()) {
+                // dereference the array to get an embedded array
+                // compile expression index -- adds 1 to height
+                idxExpr.compile(mv, compileContext);
+                // make sure the index is an integer
+                compileTypeConversion(idxExpr.getType(), Type.I, mv, compileContext);
+                // fetch embedded array pop 2 and add 1
+                mv.visitInsn(Opcodes.AALOAD);
+                compileContext.addStackCount(-1);
+                valueType = valueType.getBaseType();
+            } else {
+                if (isTwoWords) {
+                    // stack is [..., val1, val2, val1, val2, aref ] and we want [..., val1, val2, aref, val1, val2 ]
+                    mv.visitInsn(Opcodes.DUP_X2);     // ==>  [..., val1, val2, aref. val1, val2, aref ]
+                    compileContext.addStackCount(1);
+                    mv.visitInsn(Opcodes.POP);        // ==> [..., val1, val2, aref. val1, val2 ]
+                    compileContext.addStackCount(-1);
+                } else {
+                    // stack is [..., val, val, aref ] and we want [..., val, aref, val ]
+                    mv.visitInsn(Opcodes.SWAP);
+                }
+                // compile expression index -- adds 1 to height
+                idxExpr.compile(mv, compileContext);
+                // make sure the index is an integer
+                compileTypeConversion(idxExpr.getType(), Type.I, mv, compileContext);
+                if (isTwoWords) {
+                    // stack is [..., val1, val2, aref, val1, val2, idx] and we want [..., val1, val2, aref, idx, val1, val2 ]
+                    mv.visitInsn(Opcodes.DUP_X2);     // ==> [..., val1, val2, aref, idx, val1, val2, idx]
+                    compileContext.addStackCount(1);
+                    mv.visitInsn(Opcodes.POP);        // ==> [..., val1, val2, aref, idx, val1, val2 ]
+                    compileContext.addStackCount(-1);
+                } else {
+                    // stack is [..., val, aref, val, idx] and we want [..., val, aref, idx, val ]
+                    mv.visitInsn(Opcodes.SWAP);
+                }
+                // now we can do the array store
+                if (valueType.isObject() || valueType.isArray()) {
+                    // compile load object - pops 3
+                    mv.visitInsn(Opcodes.AASTORE);
+                    toPop =- 3;
+                } else if (valueType == Type.Z || valueType == Type.B) {
+                    // compile load byte - pops 3
+                    mv.visitInsn(Opcodes.BASTORE);
+                    toPop = -3;
+                } else if (valueType == Type.S) {
+                    // compile load short - pops 3
+                    mv.visitInsn(Opcodes.SASTORE);
+                    toPop = -3;
+                } else if (valueType == Type.C) {
+                    // compile load char - pops 3
+                    mv.visitInsn(Opcodes.CASTORE);
+                    toPop = -3;
+                } else if (valueType == Type.I) {
+                    // compile load int - pops 3
+                    mv.visitInsn(Opcodes.IASTORE);
+                    toPop = -3;
+                } else if (valueType == Type.J) {
+                    // compile load long - pops 4
+                    mv.visitInsn(Opcodes.LASTORE);
+                    toPop = -4;
+                } else if (valueType == Type.F) {
+                    // compile load float - pops 3
+                    mv.visitInsn(Opcodes.FASTORE);
+                    toPop = -3;
+                } else if (valueType == Type.D) {
+                    // compile load double - pops 4
+                    mv.visitInsn(Opcodes.DASTORE);
+                    toPop = -4;
+                }
+                compileContext.addStackCount(toPop);
+                if (iterator.hasNext()) {
+                    assert valueType.isArray();
+                    valueType =valueType.getBaseType();
+                }
+            }
+        }
+
+        // check stack height
+        if (compileContext.getStackCount() != currentStack) {
+            throw new CompileException("ArrayExpression.compile : invalid stack height " + compileContext.getStackCount() + " expecting " + currentStack);
+        }
+
+        // we needed room for an aray and an index or for a one or two word result
+        // but the recursive evaluations will have made sure the max stack is big enough
+        // so there is no need to update the maximum stack height
+    }
+
+    @Override
+    public void bindAssign() throws TypeException {
+        bind();
+    }
 }
