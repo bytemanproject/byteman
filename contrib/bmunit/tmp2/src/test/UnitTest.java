e8b82c2e64a9d69ae5e507f6bed6d45838dd0fbb
Andrew Dinn <adinn@redhat.com>
2010-12-18 12:40:26 +0000

    commented the example --  more fixes for BYTEMAN-144

--- contrib/bmunit/tmp2/src/test/UnitTest.java
+++ contrib/bmunit/tmp2/src/test/UnitTest.java
@@ -9,11 +9,17 @@ import org.jboss.byteman.contrib.bmunit.BMUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+// this runner is like the normal JUnit4 runner i.e. it runs all methods with an @Test annotation
+// but it also pays atentionto BMRules annotations. it will autoload the agent into the JVM if needed
 @RunWith(BMUnitRunner.class)
+// A class level annotation identifies a rule script which is loaded before any test is run and only unloaded
+// at the end of testing
 @BMRules
 public class UnitTest
 {
     @Test
+    // A method annotation identifies a rule script which is loaded before calling the tes method and
+    // then unloaded after the test has run
     @BMRules
     public void testOne()
     {
@@ -23,6 +29,7 @@ public class UnitTest
     }
 
     @Test
+    // If you supply a value then this is used when looking for the script otherwise the method name is used
     @BMRules("two")
     public void testTwo()
     {
@@ -31,10 +38,7 @@ public class UnitTest
         tryAlways();
     }
 
-    public void tryAlways()
-    {
-        // message injected by Byteman in all tests
-    }
+    // The remaining methods have code injected into by the scripts for the class and test methods
 
     public void tryOne()
     {
@@ -45,4 +49,10 @@ public class UnitTest
     {
         // message injected by Byteman in testTwo
     }
+
+    public void tryAlways()
+    {
+        // message injected by Byteman in all tests
+    }
+
 }
