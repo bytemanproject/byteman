7892026cb07eeacdf3a09e5c606e89e2995aa751
Andrew Dinn <adinn@redhat.com>
2011-07-08 11:24:40 +0000

    added bugfix test case for BYTEMAN-163

---
+++ tests/src/org/jboss/byteman/tests/bugfixes/TestStringAsCharSequence.java
@@ -0,0 +1,56 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2009, Red Hat and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*
+* @authors Andrew Dinn
+*/
+package org.jboss.byteman.tests.bugfixes;
+
+import org.jboss.byteman.tests.Test;
+
+/**
+ * Test for BYTEMAN-163 to check that a String can be passed to a method which expects a CharSequence.
+ */
+public class TestStringAsCharSequence extends Test
+{
+    public TestStringAsCharSequence()
+    {
+        super(TestStringAsCharSequence.class.getCanonicalName());
+    }
+
+    public void test()
+    {
+        compareMethod("abc");
+
+        checkOutput();
+    }
+
+    public void compareMethod(String str)
+    {
+        log("inside compareMethod()");
+    }
+
+    @Override
+    public String getExpected() {
+        logExpected("inside compareMethod()");
+        logExpected("abc contains bc");
+        return super.getExpected();
+    }
+}
