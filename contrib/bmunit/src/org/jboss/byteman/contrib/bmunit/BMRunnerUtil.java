f42e4046863d426c9bfe64fa08e6102a960a424e
Flavia Rainone <flavia.rainone@jboss.com>
2012-02-14 14:28:39 -0200

    [BYTEMAN-192] At BMRunnerUtil.constructScriptText, prepend a \n to targetLocation.

--- contrib/bmunit/src/org/jboss/byteman/contrib/bmunit/BMRunnerUtil.java
+++ contrib/bmunit/src/org/jboss/byteman/contrib/bmunit/BMRunnerUtil.java
@@ -41,11 +41,12 @@ public class BMRunnerUtil {
             builder.append(bmRule.targetMethod());
             String location = bmRule.targetLocation();
             if (location != null && location.length() > 0) {
-		// if we have AT XXX or AFTER XXX then use it
-		// otherwise prepend AT
-		if (!location.startsWith("AFTER ") && !location.startsWith("AT ") ) {
-		    builder.append("\nAT ");
-		}
+                builder.append('\n');
+                // if we have AT XXX or AFTER XXX then use it
+                // otherwise prepend AT
+                if (!location.startsWith("AFTER ") && !location.startsWith("AT ") ) {
+                    builder.append("AT ");
+                }
                 builder.append(location);
             }
             String helper = bmRule.helper();
