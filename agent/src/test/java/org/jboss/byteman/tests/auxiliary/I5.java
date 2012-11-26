19c26833739e43ca1d4add71c647b01accca0fb2
Andrew Dinn <adinn@redhat.com>
2012-11-26 17:27:22 +0000

    added test case for
    fixed problem with method compilation where it was planting an
    invokevirtual where invokeinterface was needed
    
    fixes for BYTEMAN-219

---
+++ agent/src/test/java/org/jboss/byteman/tests/auxiliary/I5.java
@@ -0,0 +1,11 @@
+package org.jboss.byteman.tests.auxiliary;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: adinn
+ * Date: 26/11/12
+ * Time: 12:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class I5 {
+}
