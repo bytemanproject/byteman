b72678f6b80204a68d0fa3e720b8305e44b4202e
Fernando Miguélez <fernando.miguelez@gmail.com>
2017-04-04 11:09:47 +0200

    [CHG] Modified versions to 3.0.9-SNAPSHOT [CHG] byteman-jboss-modules-plugin also works now with older versions of jboss-modules (1.2.0) that use protected constructors for ModuleLoader [NEW] byteman-install now accepts "-m" option to enable JBoss module plugin

--- bin/bminstall.bat
+++ bin/bminstall.bat
@@ -26,14 +26,15 @@
 @rem a JVM which was started without the agent. This provides an
 @rem alternative to using the -javaagent java command line flag
 @rem
-@rem usage: bminstall [-p port] [-h host] [-b] [-s] [-Dname[=value]]* pid
+@rem usage: bminstall [-p port] [-h host] [-b] [-s] [-m] [-Dname[=value]]* pid
 @rem   pid is the process id of the target JVM
 @rem   -h host selects the host name or address the agent listener binds to
 @rem   -p port selects the port the agent listener binds to
 @rem   -b adds the byteman jar to the bootstrap classpath
 @rem   -s sets an access-all-areas security policy for the Byteman agent code
+@rem   -m activates the byteman JBoss modules plugin
 @rem   -Dname=value can be used to set system properties whose name starts with "org.jboss.byteman."
-@rem   expects to find a byteman agent jar in BYTEMAN_HOME
+@rem   expects to find a byteman agent jar and byteman JBoss modules plugin jar (if -m is indicated) in BYTEMAN_HOME
 @rem
 @rem -----------------------------------------------------------------------------------
 if "%OS%" == "Windows_NT" setlocal
@@ -84,12 +85,13 @@ if "%OS%" == "Windows_NT" endlocal
 exit /b
 
 :showUsage
-echo usage: bminstall [-p port] [-h host] [-b] [-s] [-Dname[=value]]* pid
+echo usage: bminstall [-p port] [-h host] [-b] [-s] [-m] [-Dname[=value]]* pid
 echo   pid is the process id of the target JVM
 echo   -h host selects the host name or address the agent listener binds to
 echo   -p port selects the port the agent listener binds to
 echo   -b adds the byteman jar to the bootstrap classpath
 echo   -s sets an access-all-areas security policy for the Byteman agent code
+echo   -m activates the byteman JBoss modules plugin
 echo   -Dname=value can be used to set system properties whose name starts with "org.jboss.byteman."
-echo   expects to find a byteman agent jar in BYTEMAN_HOME
+echo   expects to find a byteman agent jar and byteman JBoss modules plugin jar (if -m is indicated) in BYTEMAN_HOME
 goto exitBatch
