c8855fbfe145b855c1e17f02ccda3fcd7aa01694
WangXiang <wx347249478@gmail.com>
2021-05-20 15:05:24 +0800

    remove useless code (#5)
    Signed-off-by: xiang <xiang13225080@163.com>

--- agent/src/main/java/org/jboss/byteman/agent/Retransformer.java
+++ agent/src/main/java/org/jboss/byteman/agent/Retransformer.java
@@ -473,17 +473,4 @@ public class Retransformer extends Transformer {
     public Set<String> getLoadedSystemJars() {
         return new HashSet<String>(sysJars); // returns a copy
     }
-
-    public static void setEnv(String key, String value) {
-        try {
-            Map<String, String> env = System.getenv();
-            Class<?> cl = env.getClass();
-            Field field = cl.getDeclaredField("m");
-            field.setAccessible(true);
-            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
-            writableEnv.put(key, value);
-        } catch (Exception e) {
-            throw new IllegalStateException("Failed to set environment variable", e);
-        }
-    }
 }
